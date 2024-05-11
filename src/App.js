83ee928106fbacb048e7fe9166b6a6a870067ccc
Saurabh Kumar Singh <singhsaurabhkumar087@gmail.com>
2024-05-11 20:21:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = ()=> {
       <React.Fragment>
         <Navbar></Navbar>
         <Routes>
-          <Route path="/" element={<ProductList />} />
+          <Route path="/free_shop" element={<ProductList />} />
           <Route path="/Details" element={<Details/>} />
           <Route path="/Cart" element={<Cart/>} />
           <Route element={<Default/>} />
