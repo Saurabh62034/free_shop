0c60caf10b4bdf4947c303081e8cb494683f5de6
Saurabh Kumar Singh <singhsaurabhkumar087@gmail.com>
2024-05-11 20:13:30 +0530

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -2,7 +2,7 @@ export const storeProducts = [
     {
         id: 1,
         title: "GooglePixel 7A - Sea",
-        img: "./img/google-pixel-7a.jpg",
+        img: "../public/img/google-pixel-7a.jpg",
         price: "39,999",
         info: "8 GB RAM | 128 GB RO",
         inCart: false,
@@ -13,7 +13,7 @@ export const storeProducts = [
     ,{
     id: 2,
     title: "Samsung galaxy f13",
-    img: "./img/Samsung galaxy f13.jpg",
+    img: "../public/img/Samsung galaxy f13.jpg",
     price: "9,999",
     info: "4 GB RAM | 64 GB ROM | Expandable Upto 256",
     inCart: false,
@@ -24,7 +24,7 @@ export const storeProducts = [
 ,{
     id: 3,
     title: "Infinix zero 30 5G",
-    img: "./img/infinix_zero30-5G.jpg",
+    img: "../public/img/infinix_zero30-5G.jpg",
     price: "13,999",
     info: "6 GB RAM | 128 GB",
     inCart: false,
@@ -35,7 +35,7 @@ export const storeProducts = [
 ,{
     id: 4,
     title: "MOTOROLA g14",
-    img: "./img/MOTOROLA g14.jpg",
+    img: "../public/img/MOTOROLA g14.jpg",
     price: '39,999',
     info: "8 GB RAM | 128 GB",
     inCart: false,
@@ -46,7 +46,7 @@ export const storeProducts = [
 ,{
     id: 5,
     title: "Poco m6-pro-5g",
-    img: "./img/Poco m6-pro-5g.jpg",
+    img: "../public/img/Poco m6-pro-5g.jpg",
     price: "15,999",
     info: "8 GB RAM | 128 GB ROM",
     inCart: false,
@@ -56,7 +56,7 @@ export const storeProducts = [
 {
     id: 6,
     title: "Motorola-edge-40-neo",
-    img: "./img/Motorola-edge-40-neo.jpg",
+    img: "../public/img/Motorola-edge-40-neo.jpg",
     price: "25,999",
     info: "8 GB RAM | 128 GB ROM",
     inCart: false,
@@ -66,7 +66,7 @@ export const storeProducts = [
 {
     id: 7,
     title: "Oppo Reno 10 5G",
-    img: "./img/Oppo Reno 10 5G.jpg",
+    img: "../public/img/Oppo Reno 10 5G.jpg",
     price: "32,999",
     info: "8 GB RAM | 128 GB ROM",
     inCart: false,
@@ -76,7 +76,7 @@ export const storeProducts = [
 {
     id: 8,
     title: "Realme GT 2 Pro",
-    img: "./img/Realme GT 2 Pro.jpg",
+    img: "../public/img/Realme GT 2 Pro.jpg",
     price: "29,999",
     info: "8 GB RAM | 128 GB ROM",
     inCart: false,
@@ -88,7 +88,7 @@ export const storeProducts = [
 export const DetailProduct = {
     id: 9,
     title: "xiaomi-poco-m5s",
-    img: "https://www.etoren.com/upload/images/0.09474300_1666160487_xiaomi-poco-m5s-dual-sim-128gb-white-6gb-ram-global-version.jpg",
+    img: "../public/img/Poco m6-pro-5g.jpg",
     price: "39,999",
     company: "google",
     info: "lorem ipsum",
